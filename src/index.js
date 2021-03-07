99cd3dca53582e848bfe5d49231ed96099a4029a
Yauheni Kashlei <one.eugene.pacific@gmail.com>
2021-03-07 15:15:08 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   if (str.length % 2 != 0) return false;
+   let l = [], k = 0;
+   for (let i = 0; i < bracketsConfig.length; i++) {
+      l[i] = bracketsConfig[i][0] + bracketsConfig[i][1];
+      k++; 
+   }
+
+   while (true) {
+      for (let i = 0; i < k*k; i++) {
+         for (let j = 0; j < k*k; j++) {
+            str = remove(str, l[j]);
+            if (str == "") return true;
+         }
+      }
+      break;
+   }
+   return false;
+}
+
+function remove(str, l) {
+   while (true) {
+      let p = str;
+      str = str.replace(l, "")
+      if (p == str || str == "") return str;
+   }
 }
